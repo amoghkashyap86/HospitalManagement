f9c9a383552caf618fd8d72c41f2ac7069eafbf9
amoghkashyap86 <amoghk87kashyap@gmail.com>
2020-11-22 17:57:09 +0530

    second commit

--- controller/index.js
+++ controller/index.js
@@ -3,6 +3,7 @@ const express = require("express")
 const bodyparser = require("body-parser")
 const app = express()
 const path = require("path")
+
 // var mysqlconnection = mysql.createConnection(
 //     {
 //         host: 'localhost',
@@ -51,6 +52,27 @@ app.post("/update", function (req, res) {
     console.log(dummyid)
     res.sendFile("D:\\dbmsprj\\public\\update.html")
 })
+app.post("/updatedata", function (req, res) {
+    pid = req.body.pid;
+    pname = req.body.pname;
+    page = req.body.page;
+    pnumber = req.body.pnumber;
+    pbloodgroup = req.body.pbloodgroup;
+    pweight = req.body.pweight;
+    pgender = req.body.gender;
+
+    paddress = req.body.paddress;
+    padmissiondate = req.body.padmissiondate
+    updatequerry = `update patient set pname='${pname}',p_age=${page},p_number=${pnumber},pgender='${pgender}',blood_group='${pbloodgroup}',weight=${pweight},address='${paddress}',admission_date='${padmissiondate}' 
+       where pno=${pid}`
+    mysqlconnection.query(updatequerry, function (err, row, field) {
+        if (err) {
+            console.log("error has occured near line 71 index.js")
+        }
+    })
+    res.send("updated")
+
+})
 app.get("/getpidsdetail", function (req, res) {
     res.setHeader("Access-Control-Allow-Origin", '*')
     mysqlconnection.query(`select * from patient where pno=${dummyid}`, function (err, row, field) {
@@ -73,14 +95,15 @@ app.post("/savedata", function (req, res) {
     pgender = req.body.gender;
 
     paddress = req.body.paddress;
+    padmissiondate = req.body.padmissiondate
     console.log(pgender)
     console.log(paddress)
-    insertquerry = `insert into  patient values (${pid},'${pname}',${page},${pnumber},'${pgender}','${pbloodgroup}',${pweight},'${paddress}')`;
+    insertquerry = `insert into  patient values (${pid},'${pname}',${page},${pnumber},'${pgender}','${pbloodgroup}',${pweight},'${paddress}','${padmissiondate}')`;
     mysqlconnection.query(insertquerry, function (err, row, fiels) {
         if (!err) {
-            console.log(row)
+            console.log(row, "ok packet thing")
         } else {
-            console.log(err)
+            console.log(err, "error near /savedata")
         }
     })
     res.send("form submited")
@@ -90,6 +113,14 @@ app.get("/getdata", function (req, res) {
     mysqlconnection.query('select * from patient;', function (err, row, fields) {
         if (!err) {
             // console.log(row.length)
+            console.log(row)
+            // console.log((row[11]["admission_date"]))
+            // d = row[11]["admission_date"]
+
+            // str = d.toISOString().split('T')[0]
+            // console.log(str)
+
+
             res.setHeader("Access-Control-Allow-Origin", '*')
             res.send(row)
         }
